645a5978f722f1b597f994fcecdd4a9dc68a3e3d
hitencodes01 <hitenunnao@gmail.com>
2025-07-01 12:27:34 +0530

    ui

--- src/index.js
+++ src/index.js
@@ -19,3 +19,11 @@ dotenv.config(
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000 , ()=>{
+        console.log("server is running on port ", process.env.PORT)
+    })
+})
+.catch((err)=>{
+    console.log("mongodb connection failed !!!" , err)
+})
